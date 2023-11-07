af3edc9a9923d55c9e33badb7dce116b88ed841d
SarNadya <lutik.ne@gmail.com>
2023-11-07 20:32:38 +0300

    настройка eslint и prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import { Suspense } from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
-import { AuthorizationPageAsync } from './pages/AuthorizationPage/AuthorizationPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Suspense } from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+import { AuthorizationPageAsync } from "./pages/AuthorizationPage/AuthorizationPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 function App() {
   return (
     <div>
-      <NavLink to={'/'} >Главная</NavLink>
-      <NavLink to={'/authorization'} >Авторизация</NavLink>
+      <NavLink to={"/"}>Главная</NavLink>
+      <NavLink to={"/authorization"}>Авторизация</NavLink>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route
-            path={'/'}
-            element={<MainPageAsync/>}
-          />
-          <Route
-            path={'/authorization'}
-            element={<AuthorizationPageAsync/>}
-          />
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/authorization"} element={<AuthorizationPageAsync />} />
         </Routes>
       </Suspense>
     </div>
