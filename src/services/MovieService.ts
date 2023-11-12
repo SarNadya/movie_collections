9daacdaeb25f94bc520b640e5b4b4da68f92e5d0
SarNadya <lutik.ne@gmail.com>
2023-11-12 15:37:13 +0300

    настройка переменных среды, добавлен файл .env

--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -1,13 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Movies } from '../types/Movies';
+import { API_KEY, API_URL } from '../constants/api';
 
 export const movieAPI = createApi({
   reducerPath: 'movieAPI',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.kinopoisk.dev/v1.4' }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
   endpoints: (build) => ({
     fetchAllMovies: build.query<Movies, number>({
-      query: (limit) =>
-        `/movie?field=rating.imdb=8-10&limit=${limit}&token=FSRTNMB-XKQMXCW-P74X3PH-D4M76BC`,
+      query: (limit) => `/movie?field=rating.imdb=8-10&limit=${limit}&token=${API_KEY}`,
     }),
   }),
 });
