4e21b185609b6d34f0d0c6ae78883c3a49208a7e
SarNadya <lutik.ne@gmail.com>
2023-11-13 17:13:34 +0300

    добавлены общие стили

---
+++ src/components/App.tsx
@@ -0,0 +1,30 @@
+import { Suspense } from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { AuthorizationPageAsync } from '../pages/AuthorizationPage/AuthorizationPage.async';
+import { MainPageAsync } from '../pages/MainPage/MainPage.async';
+import { Layout } from 'antd';
+import { Content, Header } from 'antd/es/layout/layout';
+import '../styles/index.sass';
+
+function App() {
+  return (
+    <div className="app">
+      <Layout>
+        <Header>
+          <NavLink to={'/'}>Главная</NavLink>
+          <NavLink to={'/authorization'}>Авторизация</NavLink>
+        </Header>
+        <Content>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path={'/'} element={<MainPageAsync />} />
+              <Route path={'/authorization'} element={<AuthorizationPageAsync />} />
+            </Routes>
+          </Suspense>
+        </Content>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
